4c130649a2ce0c6493c39a13c4a12cd17c5ca168
BruceBAWest <brucebawest@gmail.com>
2018-06-21 12:31:26 -0400

    add gulp watch. update some HTML
    still need to change text, images, and font colors. Also need to finish setting up gulp watch - it's not continuously running like it should.

--- gulpfile.js
+++ gulpfile.js
@@ -119,3 +119,20 @@ gulp.task('dev', ['css', 'js', 'browserSync'], function() {
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
 });
+
+//Gulp watch
+watch = require('gulp-watch');
+
+gulp.task('stream', function () {
+    // Endless stream mode
+    return watch('css/**/*.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('callback', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('css/**/*.css', function () {
+        gulp.src('css/**/*.css')
+            .pipe(gulp.dest('build'));
+    });
+});
